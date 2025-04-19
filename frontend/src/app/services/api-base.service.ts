186d3ac5ea0d1523c02ba9557904f8490121592b
jonathanmstevenson <jmstevenson101@gmail.com>
2025-04-19 06:24:54 -0400

    +basic api services

---
+++ frontend/src/app/services/api-base.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, isDevMode } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiBaseService {
+
+  private _origin!: string;
+
+  constructor() {
+    this._origin = isDevMode() ? 'http://localhost:4999' : '$(api_baseurl)';
+  }
+
+  get Origin(): Readonly<string> {
+    return this._origin;
+  }
+}
