d97af3784ce8d0805c8bf55cf00d2ee09b6fc232
jonathanmstevenson <jmstevenson101@gmail.com>
2025-04-19 06:39:48 -0400

    setories service: + get method with comp. injection

--- frontend/src/app/components/story-browser/story-browser.component.ts
+++ frontend/src/app/components/story-browser/story-browser.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StoriesService } from '../../services/stories.service';
+import { IStory } from '../../models/interfaces/story.model';
 
 @Component({
   selector: 'app-story-browser',
   templateUrl: './story-browser.component.html',
   styleUrl: './story-browser.component.scss'
 })
-export class StoryBrowserComponent {
+export class StoryBrowserComponent implements OnInit {
+  
+  pgSize: number = 20;
+  pgNumber: number = 1;
+  records: Array<IStory> = [];
+
+  constructor(private _storiesService: StoriesService) { }
+  
+  ngOnInit(): void {
+    this.getLatest();
+  }
+
+  getLatest() {
+    this._storiesService.getLatest(this.pgSize, this.pgNumber)
+      .subscribe(x => this.records = x);
+  }
 
 }
--- frontend/src/app/services/stories.service.ts
+++ frontend/src/app/services/stories.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ApiBaseService } from './api-base.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IStory } from '../models/interfaces/story.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StoriesService {
+export class StoriesService extends ApiBaseService {
 
-  constructor() { }
+  private route: string = this.Origin + '/stories';
+
+  constructor(private _http: HttpClient) {
+    super();
+  }
+
+
+  getLatest(pgSize: number, pgNumber: number, term?: string): Observable<IStory[]> {
+    var query = this.route + `?pgSize=${pgSize}&pgNumber=${pgNumber}`;
+    query += term ? `&term=${term}` : '';
+    return this._http.get<IStory[]>(query);
+  }
 }
